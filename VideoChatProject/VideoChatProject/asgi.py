1302ca58dc24e850fc2ff93999afc4b1b9149334
Olha <olusiabn@gmail.com>
2023-06-04 11:11:49 +0200

    Changed asgi.py settings.py, index.py, add urls and views

--- VideoChatProject/VideoChatProject/asgi.py
+++ VideoChatProject/VideoChatProject/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VideoChatProject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VideoChatProject.settings")
 
-application = get_asgi_application()
+"""Creating a routing configuration for Channels. A Channels routing 
+configuration is an ASGI application that is similar to a Django URLconf, 
+in that it tells Channels what code to run when an HTTP request is 
+received by the Channels server."""
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
