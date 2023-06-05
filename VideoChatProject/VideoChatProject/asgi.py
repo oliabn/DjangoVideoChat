e186a4712484bee1902fb1baa694631d7fa58dc6
Olha <olusiabn@gmail.com>
2023-06-05 21:19:04 +0200

    Corrected asgi.py

--- VideoChatProject/VideoChatProject/asgi.py
+++ VideoChatProject/VideoChatProject/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VideoChatProject.settings")
 
@@ -23,5 +25,8 @@ application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AuthMiddlewareStack(URLRouter(
+            chat.routing.websocket_urlpatterns
+        ))
     }
 )
