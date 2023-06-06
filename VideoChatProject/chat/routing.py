d5bdbed7768ec517cc2349bce38d81d1bfa1db62
Olha <olusiabn@gmail.com>
2023-06-06 17:05:38 +0200

    Add routing.py

---
+++ VideoChatProject/chat/routing.py
@@ -0,0 +1,12 @@
+""" Route for ChatConsumer
+(is for the WebSockets for ASGI support of Django).
+Routing to the URL ChatConsumer which 
+will handle the chat functionality. """
+
+
+from django.urls import path
+from .consumers import ChatConsumer
+
+websocket_urlpatterns = [
+    path("", ChatConsumer.as_asgi()),
+]
